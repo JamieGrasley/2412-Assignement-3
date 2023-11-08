301b96955fdb21e6daac918dd5d38ff59b63f19a
Jamie <16744724+JamieHour@users.noreply.github.com>
2023-11-08 02:59:15 -0500

    Updated definitions, created for loop for array -J

--- main.c
+++ main.c
@@ -11,30 +11,43 @@ Use safe pointers*/
  
 #include <stdlib.h>
 #include <stdio.h>
-#include "time.h"
-#include "math.h"
-#include "heap.h"
-#include "arrays.h"
+#include <math.h>
+#include <time.h>
 #define MAXHEAP 20;
+
+struct Heap{
+    unsigned long *arr;
+    unsigned int heaspsize, length;
+};
+
+void MAX_HEAPIFY(struct Heap *h, unsigned int i);
+void BUILD_MAX_HEAP(struct Heap *h);
+void heap_sort(struct Heap *h);
+void insertion_sort(unsigned long *arr, unsigned int length);
+void merge_sort(unsigned long *a, unsigned int left, unsigned int right);
+
+
 
 //array_size must be between 2^7 and 2^22
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    struct Heap *h;
-    unsigned int *g, *f;
-    int n = MAXHEAP + 1;
 
+    time_t insertRuntime_t=time(NULL);
+
+    struct Heap *h;
+    unsigned long *g, *f;
+    int n = MAXHEAP + 1;
+for (unsigned int array_size=128; array_size<4194304; array_size*2){
     g = NULL; f = NULL; 
     h = (struct Heap *) malloc(sizeof(struct Heap));
     
-    f = (unsigned int*) malloc(  n  * sizeof(unsigned int));
-    g = (unsigned int*) malloc(  n  * sizeof(unsigned int));
+    f = (unsigned long*) malloc(  n  * sizeof(unsigned long));
+    g = (unsigned long*) malloc(  n  * sizeof(unsigned long));
     
     if( !f || !g || !h)
         exit(EXIT_FAILURE);
     h->length = MAXHEAP; // we will not use the first array element to avoid zero-based indexing and to use a 1-based indexing instead
-    h->arr = (unsigned int*) malloc( n * sizeof(struct Heap));
+    h->arr = (long unsigned int*) malloc( n * sizeof(struct Heap));
     if(!h->arr)
         exit(EXIT_FAILURE);
 
@@ -63,7 +76,7 @@ int main(int argc, const char * argv[]) {
     time(&endInsert_t);
 
     time(&startMerge_t);
-        merge_sort(h);
+        merge_sort(f);
     time(&endMerge_t);
 
     diffHeap_t=difftime(endHeap_t,startHeap_t);
@@ -71,13 +84,24 @@ int main(int argc, const char * argv[]) {
     diffMerge_t=difftime(endMerge_t,startMerge_t);
 
     // write into a CSV; check the Excel for the structure of the coloumns
-
+if(array_size=128){
     FILE *fpt;
     fpt=fopen("Output.csv", "w+");
-    fprintf(fpt,"inputSize, f(inputSizei)=f(inputSizei-1)*4, runntime (Insertionsort), runntime (MergeSort), runntime (Heapsort)"\n);
-    fprintf(fpt,"%u, holder, %f, %f, %f", array_size, holder, diffInsert_t, diffMerge_t, diffHeap_t)\n;
+    fprintf(fpt,"inputSize, f(inputSizei)=f(inputSizei-1)*4, runntime (Insertionsort), runntime (MergeSort), runntime (Heapsort)\n");
+    fprintf(fpt,"%u, %f, %f, %f, %f\n", array_size, diffInsert_t, diffInsert_t, diffMerge_t, diffHeap_t);
     fclose (fpt);
-    
+    }
+else{
+    insertRuntime_t=insertRuntime_t*4;
+    FILE *fpt;
+    fpt=fopen("Output.csv", "w+");
+    fprintf(fpt,"inputSize, f(inputSizei)=f(inputSizei-1)*4, runntime (Insertionsort), runntime (MergeSort), runntime (Heapsort)\n");
+    fprintf(fpt,"%u, %f, %f, %f, %f\n", array_size, insertRuntime_t, diffInsert_t, diffMerge_t, diffHeap_t);
+    fclose (fpt);
+}
+
+    insertRuntime_t= diffInsert_t;
+
     printf("\nArray[1 .. Heap.Length]: ");
     for(unsigned int i=1; i<=h->length; i++)
         printf("%d ", h->arr[i]);
@@ -86,5 +110,6 @@ int main(int argc, const char * argv[]) {
 
     if(h->arr) free(h->arr);
     if(h) free(h);
+}
     return 0;
 }
