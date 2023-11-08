1b8e2702b1271bf3fb2c86cb9ee24fdb9ed1e855
Jamie <16744724+JamieHour@users.noreply.github.com>
2023-11-08 01:33:45 -0500

    Fubction coded awaiting test -Jamie

--- insertion_sort.h
+++ insertion_sort.h
@@ -2,3 +2,18 @@
 |    COMP-2412 Assignment 3    |
 | Jamie Grasley & Felix Ikokwu |
 \******************************/
+
+void insertion_sort(unsigned long *arr, unsigned int length){
+    unsigned int temp;
+    int check;
+
+    for (unsigned int x=1; x<length; x++){
+            temp=arr[x];
+            check=x-1;
+            while (check>=0 && arr[check]>temp){
+                arr[check+1]=arr[check];
+                check=check-1;
+                arr[check+1]=temp;
+            }
+    }
+}
