d836e88567129b87889641af8895a7fd48622dc2
Jamie Grasley <16744724+JamieHour@users.noreply.github.com>
2023-11-08 13:55:40 -0500

    Fixed a few issues -J

--- main.c
+++ main.c
@@ -205,10 +205,10 @@ void merge_sort(unsigned long *a, unsigned int left, unsigned int right){
 }
 
 void merge(unsigned long *a, unsigned int left, unsigned int mid, unsigned int right){
-    unsigned long n1, n2, leftArr[n1], rightArr[n2];
-
+    unsigned long n1, n2;
     n1=mid-left+1;
     n2=right-mid;
+    unsigned long leftArr[n1], rightArr[n2];
 
     for(unsigned long i=1;i<n1;i++){
         leftArr[i]=a[left+i-1];
