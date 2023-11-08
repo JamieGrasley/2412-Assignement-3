e5c5e5971e90c2d6c6a1671bf1f8de368d1b7feb
Jamie Grasley <16744724+JamieHour@users.noreply.github.com>
2023-11-08 13:27:56 -0500

    added merge() per pseudo code and comp. f(x) -J

--- merge_sort.h
+++ merge_sort.h
@@ -13,11 +13,52 @@ struct Heap{
 //p=left
 //r=right
 void merge_sort(unsigned long *a, unsigned int left, unsigned int right){
-    unsigned long n1, n2, leftArr[n1], rightArr[n2];
 
     if (left<right){
             unsigned long mid=((left+right)/2);
             merge_sort(a,left,mid);
             merge_sort(a,mid+1,right);
     }
+    //Merge
+    merge(a,left,mid,right);
+
+}
+
+merge(unsigned long *a, unsigned int left, unsigned int mid, unsigned int right);
+
+merge(unsigned long *a, unsigned int left, unsigned int mid, unsigned int right){
+    unsigned long n1, n2, leftArr[n1], rightArr[n2];
+
+    for(unsigned long i=1;i<n1;i++){
+        leftArr[i]=a[left+i-1];
+    }
+    for(unsigned long x=1; x<n2;x++){
+        rightArr[x]=a[mid+x];
+    }
+    unsigned long y=0,z=0;
+    unsigned long l=left;
+
+    while(y<n1&&z<n2){
+        if(leftArr[y]<=rightArr[z]){
+            a[l]=leftArr[y];
+            y++;
+        }
+        else{
+            a[l]=rightArr[z];
+            z++;
+        }
+        l++
+    }
+    while(y<n1){
+        a[l]=leftArr[y];
+        y++;
+        l++;
+    }
+    while(z<n2){
+        a[l]=rightArr[z];
+        z++;
+        l++;
+    }
+
+
 }
