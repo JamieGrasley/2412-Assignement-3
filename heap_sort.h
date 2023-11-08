48f1db303ec83bb27dfd9092f496b99b3e5c4828
Jamie Grasley <16744724+JamieHour@users.noreply.github.com>
2023-11-08 08:53:55 -0500

    Added a few easy references as comments -J

--- heap_sort.h
+++ heap_sort.h
@@ -2,3 +2,15 @@
 |    COMP-2412 Assignment 3    |
 | Jamie Grasley & Felix Ikokwu |
 \******************************/
+
+//Heap definition for reference
+//Remember heapsize is elements stored in arr, 
+//Length is how many elements can be in arr.
+/*  struct Heap{
+    unsigned long *arr;
+    unsigned int heaspsize, length;}
+*/
+
+void heap_sort(struct Heap *h){
+
+}
