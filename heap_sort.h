1594b2007c98d1650b160aad6278a49c3586d043
Jamie Grasley <16744724+JamieHour@users.noreply.github.com>
2023-11-08 10:33:33 -0500

    Fixed heapsort, should work. Index for 1 based -J

--- heap_sort.h
+++ heap_sort.h
@@ -10,9 +10,26 @@
     unsigned long *arr;
     unsigned int heaspsize, length;}
 */
-
+struct Heap{
+    unsigned long *arr;
+    unsigned int heaspsize, length;
+};
+//struct Heap definition must be
+//Removed when reintegrated into main, here for admin only
 void heap_sort(struct Heap *h){
-    unsigned int left, right, parent;
+    unsigned int length= h->heaspsize;
+
+    for(unsigned int i=length;i>0;i--){
+        MAX_HEAPIFY(h,i);
+    }
+    for(unsigned int i=length-1;i>1;i--){
+        unsigned int temp;
+        temp=h->arr[i];
+        h->arr[i]=h->arr[1];
+        h->arr[1]=temp;
+        MAX_HEAPIFY(h,i);
+    }
+
     
 
 }
